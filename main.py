54da5efcd034ccfe150f0b52c06c23357196ad4a
Guy-afk-opps <159080036+Guy-afk-opps@users.noreply.github.com>
2024-02-05 16:29:21 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,35 @@
+mySprite = sprites.create(img("""
+        9999999999999999
+            9999999999999999
+            99999ffffff99999
+            999ffeeeeeeff999
+            99feeeeeeeeeef99
+            9feefffeeefffef9
+            9fefeeeeeeeeeff9
+            9feeefeeeeefeef9
+            efeeeeeeeeeeeefe
+            efeeeeeefeeeeefe
+            efeeeeeeffeeeefe
+            efeeeeeeeeeeeefe
+            9ffeeffffffeeff9
+            9ffffffffffffff9
+            9fffffeeeefffff9
+            9ffffffffffffff9
+            99ffffffffffff99
+            999ffffffffff999
+            9999ffffffff9999
+            9999f11ff11f9999
+            99fff11ff11fff99
+            fffff111111fffff
+            ffffff1111ffffff
+            fffffff11fffffff
+            ffffffffffffffff
+            ffffffff1fffffff
+            ffffffffffffffff
+            ffffffffffffffff
+            ffffffff1fffffff
+            ffffffffffffffff
+    """),
+    SpriteKind.player)
+scene.set_background_color(9)
+mySprite.set_position(7, 110)
